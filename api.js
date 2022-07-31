a217011b29f5654dc77cf9cb4e441afd8fce48ca
dflaig <dflaig@stud.hs-heilbronn.de>
2022-07-31 05:00:45 +0200

    edit

--- api.js
+++ api.js
@@ -2,20 +2,22 @@ const express = require('express');
 const http = require('http')
 const app = express();
 const fs = require("fs"); // node filesystem
-const { callbackify } = require('util');
+const cors = require("cors");
 const PORT = 8080;
 
+app.use(cors())
 app.use(express.json()) //Middleware -> parse data into json
+app.use(express.urlencoded({
+    extended: true
+}))
 
 
 const animal = {
-    "animal": {
-        "id" : 0,
-        "name" : "string",
-        "weight": 0,
-        "sPower": "string",
-        "eDate": "string"
-    }
+    "id" : 0,
+    "name" : "string",
+    "weight": 0,
+    "sPower": "string",
+    "eDate": "string"
 }
 
 
@@ -23,9 +25,6 @@ var server = app.listen(
     PORT,
     () => console.log(`Server listen on http://localhost:${PORT}`)   
 )
-
-
-
 
 
 // get an all existing entries
@@ -36,41 +35,32 @@ app.get('/', (req, res) => {
         obj = JSON.parse(data);
         console.log(data);
         
-        res.end(data);
-        
-    })
-    /*
-    for (i=0; i<obj["animals"].length; i++){
-        console.log(i)
-    }
-    
-    //res.send(obj["animals"][0]["name"])
-    */
-    
+        res.end(data);   
+    })   
 });
+
 
 // create new entry
 app.post('/addAnimal', (req, res) => {
-   fs.readFile( __dirname + "/" + "animals.json", 'utf8', function (err, data) {
-    if(err){
-        console.log(err)
-    } else {
-        data = JSON.parse( data );
-        const data_len = Object.keys(data).length
-        animal["animal"]["id"] = data_len;
-        animal["animal"]["name"] = "";
-        animal["animal"]["weight"] = 0;
-        animal["animal"]["sPower"] = "";
-        animal["animal"]["eDate"] = "";
-        data["animal"+data_len] = animal["animal"];
-        //json_obj = JSON.stringify(data)
-        //console.log(json_obj)
-        fs.writeFile(__dirname + "/" + "animals.json", JSON.stringify(data), 'utf8', callbackify)
-    }
-    
-    res.end(JSON.stringify(data));
-    
-    
+   fs.readFile( __dirname + "/" + "animals.json", 'utf8', function (err, file) {
+        if(err){
+            console.log(err)
+        } else {
+            let json = JSON.parse( file );
+            const data_len = json.data.length
+            animal["id"] = data_len;
+            animal["name"] = req.body.name;
+            animal["weight"] = req.body.weight;
+            animal["sPower"] = req.body.sPower;
+            animal["eDate"] = req.body.eDate;
+            json.data.push(animal);
+            //json_obj = JSON.stringify(data)
+            //console.log(json_obj)
+            fs.writeFile(__dirname + "/" + "animals.json", JSON.stringify(json, null, 4), 'utf8', function(){
+                console.log('successfull')
+            })
+            res.end(JSON.stringify(json));
+        }
     });
 });
 
@@ -98,3 +88,4 @@ app.delete('/deleteAnimal/:id', (req, res) => {
      });
      
 });
+
