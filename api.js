9a5cf88c76f81e0b395ef69fb0135fae429f815a
dflaig <dflaig@stud.hs-heilbronn.de>
2022-08-01 03:11:05 +0200

    edit

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const http = require('http')
 const app = express();
 const fs = require("fs"); // node filesystem
 const cors = require("cors");
@@ -7,7 +6,7 @@ const PORT = 8080;
 
 app.use(cors())
 app.use(express.json()) //Middleware -> parse data into json
-app.use(express.urlencoded({
+app.use(express.urlencoded({  // for reading the form data in the req body  -> otherwise form data always received as an empty array on server
     extended: true
 }))
 
@@ -30,32 +29,33 @@ var server = app.listen(
 // get an all existing entries
 app.get('/', (req, res) => {
     fs.readFile( __dirname + "/" + "animals.json", 'utf8', function (err, data) {
-        if(err) throw err;
-        //res.send(data);
+        if(err) {
+            console.log(err);
+        } else {
         obj = JSON.parse(data);
-        console.log(data);
-        
-        res.end(data);   
+        //console.log(data);
+        res.end(data);  
+        } 
     })   
 });
 
 
 // create new entry
 app.post('/addAnimal', (req, res) => {
+    console.log(req)
    fs.readFile( __dirname + "/" + "animals.json", 'utf8', function (err, file) {
         if(err){
             console.log(err)
         } else {
             let json = JSON.parse( file );
             const data_len = json.data.length
+            console.log('test')
             animal["id"] = data_len;
             animal["name"] = req.body.name;
             animal["weight"] = req.body.weight;
             animal["sPower"] = req.body.sPower;
             animal["eDate"] = req.body.eDate;
             json.data.push(animal);
-            //json_obj = JSON.stringify(data)
-            //console.log(json_obj)
             fs.writeFile(__dirname + "/" + "animals.json", JSON.stringify(json, null, 4), 'utf8', function(){
                 console.log('successfull')
             })
@@ -65,27 +65,56 @@ app.post('/addAnimal', (req, res) => {
 });
 
 // update entry
-app.post('/updateAnimal', (req, res) => {
-
-    fs.readFile( __dirname + "/" + "animals.json", 'utf8', function (err, data) {
-     data = JSON.parse( data );
-     data["animal3"] = animal["animal"];
-     console.log(Object.keys(data).length);
-     res.end( JSON.stringify(data));
-     });
+app.post('/updateAnimal/:id', (req, res) => {
+    let { id } = req.params
+    console.log(id)
+    fs.readFile( __dirname + "/" + "animals.json", 'utf8', function (err, file) {
+        if(err){
+            console.log(err)
+        } else {
+            
+            let json = JSON.parse( file );
+            let findAnimal = json.data.find(x => x.id === parseInt(id));
+            findAnimal["name"] = req.body.name
+            findAnimal["weight"] = req.body.weight
+            findAnimal["sPower"] = req.body.sPower
+            findAnimal["eDate"] = req.body.eDate
+            fs.writeFile(__dirname + "/" + "animals.json", JSON.stringify(json, null, 4), 'utf8', function(){
+                console.log('successfull')
+            })
+            res.end(JSON.stringify(json));
+        }
+    });
  });
 
 
 // delete an existing entry
 app.delete('/deleteAnimal/:id', (req, res) => {
-    const {id} = req.params
-    fs.readFile( __dirname + "/" + "animals.json", 'utf8', function (err, data) {
-        data = JSON.parse( data );
-        delete data["animal" + id];
-         
-        console.log( data );
-        res.end( JSON.stringify(data));
+    let { id } = req.params
+    fs.readFile( __dirname + "/" + "animals.json", 'utf8', function (err, file) {
+        let json = JSON.parse( file );
+        console.log(json)
+        json.data[id].delete
+        //delete json.data[id];
+        
+        console.log(json)
+        /*
+        for (i=0; i < json.data.length; i++){
+            //console.log('check')
+            if(json.data[i] === null){
+                
+            }
+            console.log(json);
+        
+
+        fs.writeFile(__dirname + "/" + "animals.json", JSON.stringify(json, null, 4), 'utf8', function(){
+            console.log('successfull')
+        })
+        */
+        res.end( JSON.stringify(json));
+        
      });
+    
      
 });
 
--- index.js
+++ index.js
@@ -7,6 +7,18 @@ $(function () {
             { data: "weight" },
             { data: "sPower" },
             { data: "eDate" },
+            {
+              data: null,
+              className: "dt-center editor-edit",
+              defaultContent: '<i class="fa fa-pencil"/>',
+              orderable: false
+            },
+            {
+              data: null,
+              className: "dt-center editor-delete",
+              defaultContent: '<i class="fa fa-trash"/>',
+              orderable: false
+            },
         ]
     });
     $('#input-form').hide('slow');
@@ -17,25 +29,76 @@ $(function () {
 
 
     $("#input-form").on("submit", function( event ) {
-      const d_arr = $('#input-form').serializeArray();
-      console.log(JSON.stringify(d_arr));
+      const animal_new = $('#input-form').serializeArray();
+      //console.log(JSON.stringify(animal_new));
       $.ajax({
         type: 'POST',
         url: 'http://localhost:8080/addAnimal',
-        data: d_arr,
+        data: animal_new,
         dataType: "json",     
         success: function() {
             $('#input-form').hide('slow');
             $('#add-an-animal-btn').show('slow');
             $('#input-form').trigger("reset");
             table.ajax.reload();
+            alert("Successfully added an entry")
         },
         error: function() {
           alert('error')
         }
-
       })
-      
       event.preventDefault();
     });
+
+
+    // Delete a record
+    $('#animal-table').on('click', 'td.editor-delete', function (e) {
+      e.preventDefault();
+      const id_del = $(this).closest('tr').children('td:first').text()
+      $.ajax({
+        type: 'DELETE',
+        url: 'http://localhost:8080/deleteAnimal/'+id_del,    
+        success: function() {
+          table.ajax.reload();
+        },
+        error: function() {
+          alert('error')
+        }
+      })
+
+    });
+
+    var id_edit;
+    // Edit a record
+    $('#animal-table').on('click', 'td.editor-edit', function (e) {
+      e.preventDefault();
+      id_edit = $(this).closest('tr').children('td:first').text()
+      console.log(id_edit)
+      $('#myModal').show()
+      
+    });
+    $("#update-form").on("submit", function( event ) {
+      const animal_update = $('#update-form').serializeArray();
+      console.log(animal_update)
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/updateAnimal/'+id_edit,
+        data: animal_update,
+        dataType: "json",    
+        success: function() {
+          table.ajax.reload();
+          alert('Successfully updated the entry')
+        },
+        error: function() {
+          alert('error')
+        }
+      })
+    })
 });
+
+
+$('#close').on('click', function(){
+  $('#myModal').hide()
+})
+
+
